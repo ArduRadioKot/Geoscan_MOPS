bf0d270ec69d654227577af041dd5af3f35b3cc2
AlexVorob <sana20112009@gmail.com>
2025-11-26 15:55:23 +0300

    test frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/header'
+import Sidebar from './components/leftPanel/leftPanel'
+import ImageViewer from './components/imageViewer/imageViewer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState(null)
+
+  const handleImageUpload = (file) => {
+    setImage(file)
+  }
+
+  const handleClearImage = () => {
+    setImage(null)
+  }
 
   return (
-   <div>
-
+   <div className="app">
+      <Header name="Name" />
+      <Sidebar onImageUpload={handleImageUpload} onClearImage={handleClearImage} />
+      <ImageViewer image={image} />
    </div>
   )
 }
